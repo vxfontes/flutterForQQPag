3e8f69f9ae4f19187121f78c9aaca2a62fb07120
vxfontes <nessa1vane@icloud.com>
2023-12-16 14:04:39 -0300

    feat: mudando tema e fonte da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
@@ -7,11 +8,20 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.grey,
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            backgroundColor: Colors.black,
+            titleTextStyle: TextStyle(color: Colors.white),
+          ),
+          textTheme: GoogleFonts.bitterTextTheme()),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
