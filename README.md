4d09678088029c238760dc8d4c00c2637d54cc75
vxfontes <nessa1vane@icloud.com>
2023-12-25 23:00:31 -0300

    feat: criando componentes principais da aplicação

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-# alubank
+# Releitura de aplicativo de banco
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+(acesse)[https://www.figma.com/file/qmOZR81uY7gb8I0wMLUFCu/Alubank?type=design&node-id=0-1&mode=design]
--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -1,3 +1,4 @@
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
@@ -7,40 +8,29 @@ class Header extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: <Color>[
-              Color.fromRGBO(103, 99, 234, 1.0),
-              Color.fromRGBO(155, 105, 254, 1.0),
-              Color.fromRGBO(195, 107, 255, 1.0),
-            ]),
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
-      ),
-      child: const Padding(
-        padding: EdgeInsets.fromLTRB(16, 80, 16, 16),
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: ThemeColors.headerGradient),
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 80, 16, 16),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text.rich(TextSpan(
-                    text: '\$',
-                    style: TextStyle(fontSize: 16),
-                    children: [
-                      TextSpan(
-                          text: '1000.00',
-                          style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold))
-                    ])),
-                Text(
-                  'Balanço disponível',
-                  style: TextStyle(fontSize: 16),
-                ),
+                Text.rich(TextSpan(text: '\$', children: [
+                  TextSpan(
+                    text: '1000.00',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ])),
+                const Text('Balanço disponível'),
               ],
             ),
-            Icon(
+            const Icon(
               Icons.account_circle,
               size: 42,
             ),
