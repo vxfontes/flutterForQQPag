b5e2a7e0ff381d7c6bd6c5cd5e7023a8b60e54a8
vxfontes <nessa1vane@icloud.com>
2024-01-02 09:09:48 -0300

    feat: adicionando curso novo

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
+import 'package:client_control/pages/client_types_page.dart';
 import 'package:flutter/material.dart';
-import 'package:panucci_ristorante/screens/home.dart';
+import 'pages/clients_page.dart';
 
 void main() {
-  runApp(const PanucciRistorante());
+  runApp(const MyApp());
 }
 
-class PanucciRistorante extends StatelessWidget {
-const PanucciRistorante({ Key? key }) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Panucci Ristorante",
-      theme: ThemeData(colorSchemeSeed: Colors.purple, useMaterial3: true),
-      home: const Home()
+      title: 'Controle de clientes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const ClientsPage(title: 'Clientes'),
+        '/tipos': (context) => const ClientTypesPage(title: 'Tipos de cliente'),
+      },
     );
   }
 }
