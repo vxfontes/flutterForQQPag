ca506272214e6c26d35e07f41b51fda5b59c1e2a
vxfontes <nessa1vane@icloud.com>
2024-01-02 19:48:06 -0300

    feat: adicionando projeto de testes

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,25 @@
-import 'package:client_control/models/client_type.dart';
-import 'package:client_control/models/clients.dart';
-import 'package:client_control/models/types.dart';
-import 'package:client_control/pages/client_types_page.dart';
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'pages/clients_page.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => Clients(clients: [])),
-      ChangeNotifierProvider(create: (context) => Types(types: [
-        ClientType(name: 'Platinum', icon: Icons.credit_card),
-        ClientType(name: 'Golden', icon: Icons.card_membership),
-        ClientType(name: 'Titanium', icon: Icons.credit_score),
-        ClientType(name: 'Diamond', icon: Icons.diamond),
-      ])),
-    ],
-    child: const MyApp(),
-  ));
+  runApp(BytebankApp());
 }
 
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class BytebankApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Controle de clientes',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const ClientsPage(title: 'Clientes'),
-        '/tipos': (context) => const ClientTypesPage(title: 'Tipos de cliente'),
-      },
+      home: Dashboard(),
     );
   }
 }
