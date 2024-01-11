dd5b27030e81e9deeb4881b722c79de285679c6c
vxfontes <nessa1vane@icloud.com>
2024-01-11 10:15:08 -0300

    feat: adicionando curso correto

---
+++ curso08/lib/models/clients.dart
@@ -0,0 +1,20 @@
+import 'package:client_control/models/client.dart';
+import 'package:flutter/material.dart';
+
+class Clients extends ChangeNotifier {
+  List<Client> clients;
+
+  Clients({
+    required this.clients
+  });
+
+  void add(Client client) {
+    clients.add(client);
+    notifyListeners();
+  }
+
+  void remove(int index) {
+    clients.removeAt(index);
+    notifyListeners();
+  }
+}
