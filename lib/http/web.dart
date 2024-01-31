07c881ef788542a68b002a32f0ffd4352f0c2e06
vxfontes <nessa1vane@icloud.com>
2024-01-31 10:38:15 -0300

    feat: adicionando conexão do dispositivo e firebase

--- lib/http/web.dart
+++ lib/http/web.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:meetups/models/device.dart';
 import 'package:meetups/models/event.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 const String baseUrl = 'http://192.168.0.110:8081/api';
 
@@ -15,6 +17,27 @@ Future<List<Event>> getAllEvents() async {
   else {
     throw Exception('Falha ao carregar os eventos');
   }
+}
 
+// Cadastra um novo dispositivo na aplicação web
+void sendDevice(Device device) async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/devices'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'token': device.token ?? '',
+      'modelo': device.model ?? '',
+      'marca': device.brand ?? ''
+    }),
+  );
 
+  // Valida se o token já foi enviado ou não
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs.setString('pushToken', device.token!);
+  prefs.setBool('tokenSent', false);
+
+  if (response.statusCode != 200) throw Exception('Falha para criar o dispositivo');
+  else prefs.setBool('tokenSent', true);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
-import 'package:meetups/screens/events_screen.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() => runApp(App());
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:meetups/http/web.dart';
+import 'package:meetups/models/device.dart';
+import 'package:meetups/screens/events_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  String? token = await messaging.getToken();
+  print('TOKEN: $token');
+
+  setPushToken(token);
+
+  runApp(App());
+}
+
+void setPushToken(String? token) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? prefsToken = prefs.getString('pushToken');
+  bool? prefSent = prefs.getBool('tokenSent');
+
+  if (prefsToken != token || (prefsToken == token && prefSent == false)) {
+    print('enviando token');
+
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String? brand;
+    String? model;
+
+    if (Platform.isAndroid) {
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      brand = androidInfo.brand;
+      model = androidInfo.model;
+    } else if (Platform.isIOS) {
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      brand = 'Apple';
+      model = iosInfo.utsname.machine;
+    }
+
+    Device device = Device(token: token, brand: brand, model: model);
+    sendDevice(device);
+  }
+
+}
 
 class App extends StatelessWidget {
   @override
